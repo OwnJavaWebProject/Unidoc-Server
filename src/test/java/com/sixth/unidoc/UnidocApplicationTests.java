45e94a57329e68502f3e812f9274916eaa5b20ec
dccif <heyujieboy@163.com>
2018-05-09 14:13:56 +0800

    init project

---
+++ src/test/java/com/sixth/unidoc/UnidocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sixth.unidoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnidocApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
