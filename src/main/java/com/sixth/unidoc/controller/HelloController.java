e999281514b1b4e658320384d8d9fc6579146a43
dccif <heyujieboy@163.com>
2018-05-11 21:43:26 +0800

    add some learn demo

--- src/main/java/com/sixth/unidoc/controller/HelloController.java
+++ src/main/java/com/sixth/unidoc/controller/HelloController.java
@@ -1,13 +1,31 @@
 package com.sixth.unidoc.controller;
 
+import com.sixth.unidoc.domain.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Hello";
+    @GetMapping("/Person")
+    public String person(Model model) {
+        Person single = new Person("he", 11);
+
+        List<Person> people = new ArrayList<>();
+        Person p1 = new Person("xx", 11);
+        Person p2 = new Person("yy", 22);
+        Person p3 = new Person("zz", 33);
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+
+        model.addAttribute("singlePerson", single);
+        model.addAttribute("people", people);
+
+        return "Person";
     }
 }
